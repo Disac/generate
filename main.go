4c49915a9195a0393f5b81e7ece7de40d3ed3d72
dongchao <dongchao@3commas.cn>
2020-02-27 17:20:37 +0800

    update

--- main.go
+++ main.go
@@ -5,16 +5,13 @@ import (
 	"git-admin.inyuapp.com/feature/generate/gen"
 )
 
-var config = flag.String("config", "./config.json", "")
-
 const defaultPath = "./config.json"
+
+var config = flag.String("config", defaultPath, "-config=./config.json")
 
 func main() {
 	flag.Parse()
 	path := *config
-	if path == "" {
-		path = defaultPath
-	}
 	g := gen.NewGenerator(path,
 		//gen.CloseGenerateConfigParseCode(),
 		//gen.CloseGenerateConfigFile(),
